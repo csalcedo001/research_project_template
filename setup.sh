09cf6b508a74dff01db2bb8ba9bc6f2d02820b46
César Salcedo <cesar.salcedo@utec.edu.pe>
2021-02-16 18:07:10 -0500

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create folders, execute one-run files (e.g. setup python package), etc.
+
+# Setup python package
+# cd package
+# python3 setup.py sdist bdist_wheel
+# pip3 install -e .
+# cd ..
